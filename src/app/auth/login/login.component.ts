0ba93d3fca5a3de72c58b7bc9c51eec65226f84c
Eric Liu <8850887+EricLiu8539@users.noreply.github.com>
2021-03-22 00:48:46 +1100

    add action creators
    add action creators

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,14 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
-import {Store} from "@ngrx/store";
+import {Store} from '@ngrx/store';
 
-import {AuthService} from "../auth.service";
-import {tap} from "rxjs/operators";
-import {noop} from "rxjs";
-import {Router} from "@angular/router";
+import {AuthService} from '../auth.service';
+import {tap} from 'rxjs/operators';
+import {noop} from 'rxjs';
+import {Router} from '@angular/router';
+import {AppState} from '../../reducers';
+import {login} from "../reducers/auth.actions";
 
 @Component({
   selector: 'login',
@@ -20,7 +22,8 @@ export class LoginComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private auth: AuthService,
-    private router: Router) {
+    private router: Router,
+    private store: Store<AppState>) {
 
     this.form = fb.group({
       email: ['test@angular-university.io', [Validators.required]],
@@ -34,7 +37,23 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+    const value = this.form.value;
+    this.auth.login(value.email, value.password)
+      .pipe(
+        tap(user => {
+          console.log(user);
+          // this.store.dispatch({
+          //   type: 'Login Action',
+          //   payload: {
+          //     userProfile: user
+          //   }
+          // });
 
+          this.store.dispatch(login({user}));
+          this.router.navigateByUrl('/courses');
+        })
+      )
+      .subscribe(noop, error => alert('Login Failed'));
   }
 
 }
