a96b853a2dc9105f4b384d0ecde78b1433986750
Eric Liu <liuzhao8539@gmail.com>
2021-05-17 21:36:17 +1000

    add login effects
    login and logout effects

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -12,6 +12,7 @@ import {EffectsModule} from '@ngrx/effects';
 import * as fromAuth from './reducers';
 import {authReducer} from './reducers';
 import {AuthGuard} from './auth.guard';
+import {AuthEffects} from './auth.effects';
 
 @NgModule({
   imports: [
@@ -24,7 +25,7 @@ import {AuthGuard} from './auth.guard';
     // StoreModule.forFeature('auth', fromAuth.reducers, { metaReducers: fromAuth.metaReducers }),
     // StoreModule.forFeature('auth', fromAuth.reducers),
     StoreModule.forFeature('auth', authReducer),
-
+    EffectsModule.forFeature([AuthEffects])
   ],
   declarations: [LoginComponent],
   exports: [LoginComponent]
