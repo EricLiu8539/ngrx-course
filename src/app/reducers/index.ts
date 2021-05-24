930bb906b483a96de5dd971f897c756cf6f3141c
Eric Liu <liuzhao8539@gmail.com>
2021-05-24 22:37:23 +1000

    add metareducer
    add metareducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -5,7 +5,7 @@ import {
   createSelector,
   MetaReducer
 } from '@ngrx/store';
-import { environment } from '../../environments/environment';
+import {environment} from '../../environments/environment';
 import {routerReducer} from '@ngrx/router-store';
 
 export interface AppState {
@@ -16,5 +16,12 @@ export const reducers: ActionReducerMap<AppState> = {
   router: routerReducer
 };
 
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log('state before: ', state);
+    console.log('action after: ', action);
+    return reducer(state, action);
+  };
+}
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [logger] : [];
